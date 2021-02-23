3dc3e8d0ebb6f6269f20b9f79995b4961a85f584
mayone-du <cocomayo1201@icloud.com>
2021-02-23 09:32:51 +0900

    add @types/express

--- src/pages/api/test.tsx
+++ src/pages/api/test.tsx
@@ -1,6 +1,3 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.status(200).json({ text: 'Hello' })
-}
-
+import {express} from "express";
